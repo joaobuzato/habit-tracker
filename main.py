0e56ec0443729c0964663cf75172b514fbd9e171
Buzato <joaobuzato@gmail.com>
2022-05-01 19:27:28 -0300

    creating a chart for ciclying

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import requests
 import os
+USERNAME = "joaobuzato"
+TOKEN = os.environ.get("PIXELA_TOKEN")
+
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": os.environ.get("PIXELA_TOKEN"),
-    "username" : "joaobuzato",
+    "token": TOKEN,
+    "username" : USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(PIXELA_ENDPOINT, json=user_params)
-print(response)
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "ciclying",
+    "name": "Ciclyng Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "momiji"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
